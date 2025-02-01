0b2bdb67db7fc871b1979b9268624c98fc9db334
Mosab N. R. AbuMoammar <mosab.abumoammar@gmail.com>
2025-02-01 12:35:58 +0200

    game manager & inputs

---
+++ Assets/hvo/Scripts/Managers/GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class GameManager : SingletonManager<GameManager>
+{
+    private Vector2 m_InitialTouchPosition;
+
+    void Update()
+    {
+        Vector2 inputPosition = Input.touchCount > 0 ? Input.GetTouch(0).position : Input.mousePosition;
+
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
+        {
+            m_InitialTouchPosition = inputPosition;
+        }
+        if (Input.GetMouseButtonUp(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended))
+        {
+            if (Vector2.Distance(m_InitialTouchPosition, inputPosition) < 10)
+            {
+                DeteckClick(inputPosition);
+            }
+        }
+    }
+
+    void DeteckClick(Vector2 inputPosition)
+    {
+        Debug.Log(inputPosition);
+    }
+}
--- Assets/hvo/Scripts/Units/Unit.cs
+++ Assets/hvo/Scripts/Units/Unit.cs
@@ -3,7 +3,9 @@ using UnityEngine;
 public abstract class Unit : MonoBehaviour
 {
     public bool IsMoving;
+
     protected Animator m_Animator;
+
     protected void Awake()
     {
         if (TryGetComponent<Animator>(out var animator))
